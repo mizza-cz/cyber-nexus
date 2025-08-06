7fb4b0d34f95e9b96a7aeb1a88170199b325302d
mizza-cz <mizza.cz666@gmail.com>
2025-08-06 10:09:35 +0200

    add fixes

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -15,3 +15,11 @@ function headerNavOpenerClick() {
     }
   });
 }
+window.addEventListener('scroll', function () {
+  const header = document.querySelector('.header');
+  if (window.scrollY > 30) {
+    header.classList.add('scrolled');
+  } else {
+    header.classList.remove('scrolled');
+  }
+});
